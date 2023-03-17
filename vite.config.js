651e874e382db7d558e2ff7bf5de3178f19b7cf5
DHoper <gxgemini777@gmail.com>
2023-03-17 16:39:02 +0800

    path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
 
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
+  base: "/headphone/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  plugins: [vue()],
-  base: "/ecommerce-website/"
-})
+});
