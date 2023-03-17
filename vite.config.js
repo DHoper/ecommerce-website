ef634ee11a5ed63f0f90b6011946ba3e715c00d0
DHoper <gxgemini777@gmail.com>
2023-03-17 16:10:02 +0800

    path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
   plugins: [vue()],
-  publicPath: 'base:/ecommerce-website/'
-});
+  base: "/ecommerce-website/"
+})
